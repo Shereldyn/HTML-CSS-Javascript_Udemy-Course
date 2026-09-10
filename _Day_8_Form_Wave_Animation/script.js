e76b0d8935d87a50e542a55fcd3d62c551a2e9bc
shereldyn <144779066+Shereldyn@users.noreply.github.com>
2026-09-10 23:02:47 +0200

    minor change

--- _Day_8_Form_Wave_Animation/script.js
+++ _Day_8_Form_Wave_Animation/script.js
@@ -1,7 +1,7 @@
 const labels = document.querySelectorAll('.form-control label');
 
 labels.forEach(label => {
-    label.innerHTML = label.innerText  //sets the inner HTML to
+    label.innerHTML = label.innerText
         .split('')
         .map((letter, idx) => `<span style="transition-delay:${idx * 50}ms">${letter}</span>`)
         .join('')
